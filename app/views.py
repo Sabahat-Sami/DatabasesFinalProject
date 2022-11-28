d2cad86f9c7a6b7b39c21d4ec5e8a7eb61960990
zonayedrahman <zr578@nyu.edu>
2022-11-27 20:35:51 -0500

    Updated home page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for,session
-
-
-
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for, session
 
 views = Blueprint('views', __name__)
 
@@ -9,6 +6,18 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 # @login_required
 def home():
-    print(session)
-    return render_template('home.html', user=session)
+    if request.method == "POST":
+        source = request.form.get('source')
+        destination = request.form.get('destination')
+        date = request.form.get('date')
 
+        search = []
+        ############################
+        # fetch data and display on front page(probably collect in a list and on home.html
+        # if list not empty, then display
+        ############################
+        if not search:
+            flash("No flights found!", category='error')
+        return render_template('home.html', user=session, search=search)
+
+    return render_template('home.html', user=session, search=None)
